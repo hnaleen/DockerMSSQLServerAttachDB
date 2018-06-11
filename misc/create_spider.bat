de9542ac431010d324b346e1b28cf10557f348c0
Hasantha Alahakoon <hasantha.alahakoon@cambio.se>
2018-06-11 11:49:08 +0530

    added some comments

--- misc/create_spider.bat
+++ misc/create_spider.bat
@@ -1,5 +1,4 @@
-REM wait for the SQL server to start
+REM give some time for the sql server to start
 ping -n 10 127.0.0.1 >nul
 
-REM TODO - replace with simple sqlcmd
-"C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE" -S localhost,1433 -U sa -P Nova@7610 -d master -i c:\\nova\\spider.sql
+sqlcmd -S localhost,1433 -U sa -P Nova@7610 -d master -i c:\\nova\\spider.sql
